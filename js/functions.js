005b836c8d5e8e13fa84366a5e9c2f2a93d7b089
Dmoldd <oldtime_tula@mail.ru>
2023-02-20 13:50:07 +0300

    Написал три функции

--- js/functions.js
+++ js/functions.js
@@ -1,20 +1,33 @@
-const CURRENT_PHRASE = 'леша на полке клопа нашел';
-const AVAILABLE_LENGTH = 20;
+// Функция проверки длины строки
 
-function checkLenght(stringLenght) {
-  stringLenght = CURRENT_PHRASE.length;
-  return stringLenght <= AVAILABLE_LENGTH ? 'Строка имеет допустимую длину' : 'Строка слишком длинная';
+function checkLength(currentPhrase, availableLength) {
+  const stringLength = currentPhrase.length;
+  return stringLength <= availableLength ? 'Строка имеет допустимую длину' : 'Строка слишком длинная';
 }
 
-checkLenght();
+checkLength('Введите Ваш текст', 20);
 
-function checkPhalindrome(enteredPrase) {
-  enteredPrase = CURRENT_PHRASE.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
+// Функция проверки строки на палиндром
+// ВОПРОС: почему при сортировке программа не видит букву Ё?
+
+function checkPhalindrome(currentPhrase) {
+  const text = currentPhrase.toLowerCase().replace(/[^а-яa-z1-9]/gi, '');
   let result = '';
-  for (let i = (enteredPrase.length - 1); i >= 0; i--) {
-    result += enteredPrase[i];
+  for (let i = (text.length - 1); i >= 0; i--) {
+    result += text[i];
   }
-  return enteredPrase === result ? 'Строка является палиндромом' : 'Строка не является палиндромом';
+  return text === result ? 'Строка является палиндромом' : 'Строка не является палиндромом';
 }
 
-checkPhalindrome(); // Вопрос: почему при сортировке программа не видит букву Ё?
+checkPhalindrome('Леша на полке клопа нашел');
+
+// Функция извлечения цифр их строки
+
+function extractNumber(currentPhrase) {
+  const phrase = currentPhrase.replace(/[^0-9]/g,'');
+  return phrase;
+}
+
+extractNumber('19 февряла 2023 года');
+
+// Функция трансформации строки до минимальной длины при помощи добавления указанного символа
