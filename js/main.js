4e3a62a50568f1a43c1a447fdd0a7d32a44f405d
Dmoldd <oldtime_tula@mail.ru>
2023-03-04 17:42:20 +0300

    Исправил названия функций и подредактировал массивы

--- js/main.js
+++ js/main.js
@@ -2,12 +2,37 @@ const POST_COUNT = 25;
 const COMMENTS_COUNT = 2;
 const LIKES_MIN_COUNT = 15;
 const LIKES_MAX_COUNT = 200;
-const NAMES = ['Дарья', 'Вадим', 'Екатерина', 'Дмитрий', 'Елена', 'Сергей', 'Софья', 'Константин', 'Зоя', 'Александр'];
-const COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const DESCRIPTIONS = ['Эту фотографию я сделал сам! #me', 'С друзьями на шашлыках #отдых', 'Это мы на отдыхе #отдыхаемхорошо #веселоидружно', 'Собрались со всей семьей дома #милыйдом #отдыхаем #уютный вечер', 'Еду домой! #милыйдом #уютный вечер'];
+const NAMES = [
+  'Дарья',
+  'Вадим',
+  'Екатерина',
+  'Дмитрий',
+  'Елена',
+  'Сергей',
+  'Софья',
+  'Константин',
+  'Зоя',
+  'Александр'
+];
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const DESCRIPTIONS = [
+  'Эту фотографию я сделал сам! #me',
+  'С друзьями на шашлыках #отдых',
+  'Это мы на отдыхе #отдыхаемхорошо #веселоидружно',
+  'Собрались со всей семьей дома #милыйдом #отдыхаем #уютныйвечер',
+  'Еду домой! #милыйдом #уютныйвечер',
+  'Как здорово получилось! #Супер'
+];
 
 // Функция-генератор случайного числа
-const GET_RANDOM_INTEGER = (min, max) => {
+const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(min, max));
   const upper = Math.floor(Math.max(min, max));
   const result = Math.random() * (upper - lower + 1) + lower;
@@ -15,43 +40,40 @@ const GET_RANDOM_INTEGER = (min, max) => {
 };
 
 // Функция-генератор пути к фотографии
-const CREATE_PHOTO_URL = () => {
+const createPhotoUrl = () => {
   const WAY = 'photos/';
-  const ID = GET_RANDOM_INTEGER(1, 25);
+  const ID = getRandomInteger(1, 25);
   const EXTENSION = '.jpg';
   return WAY + ID + EXTENSION;
 };
 
 // Функция-генератор пути к аватару
-const CREATE_AVATAR_URL = () => {
+const createAvatarUrl = () => {
   const WAY = 'img/avatar-';
-  const ID = GET_RANDOM_INTEGER(1, 25);
+  const ID = getRandomInteger(1, 25);
   const EXTENSION = '.svg';
   return WAY + ID + EXTENSION;
 };
 
 // Функция-генератор объекта комментария
-const GET_COMMENT = () => ({
-  id: GET_RANDOM_INTEGER(0, 999999999),
-  avatar: CREATE_AVATAR_URL(),
-  message: COMMENTS[GET_RANDOM_INTEGER(0, COMMENTS.length - 1)],
-  name: NAMES[GET_RANDOM_INTEGER(0, NAMES.length - 1)]
+const createComment = () => ({
+  id: getRandomInteger(0, 999999999),
+  avatar: createAvatarUrl(),
+  message: COMMENTS[getRandomInteger(0, COMMENTS.length - 1)],
+  name: NAMES[getRandomInteger(0, NAMES.length - 1)]
 });
 
-// Функция-генератор массива комментариев
-const GET_ALL_COMMENTS = Array.from({length: COMMENTS_COUNT}, GET_COMMENT);
-
 // Функция-генератор объекта публикации
-const GET_USER_POST = () => ({
-  id: GET_RANDOM_INTEGER(1, 25),
-  url: CREATE_PHOTO_URL(),
-  description: DESCRIPTIONS[GET_RANDOM_INTEGER(0, DESCRIPTIONS.length - 1)],
-  likes: GET_RANDOM_INTEGER(LIKES_MIN_COUNT, LIKES_MAX_COUNT),
-  comments: GET_ALL_COMMENTS
+const createUserPost = () => ({
+  id: getRandomInteger(1, 25),
+  url: createPhotoUrl(),
+  description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+  likes: getRandomInteger(LIKES_MIN_COUNT, LIKES_MAX_COUNT),
+  comments: Array.from({length: COMMENTS_COUNT}, createComment)
 });
 
 // Финальный массив
-const MASSIV_POST = Array.from({length: POST_COUNT}, GET_USER_POST);
+const MASSIV_POST = Array.from({length: POST_COUNT}, createUserPost);
 
 // eslint-disable-next-line no-console
 console.log(MASSIV_POST);
